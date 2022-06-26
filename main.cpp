ad76001a57f57e1f065a9fbbb078d9866a790f4f
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 00:54:05 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include iostream.h
+
+int main()
+{  
+   double radius = 11;  /* Centimeters
+   double pi  = 3.14
+   double sphere_volume = (4 / 3)  * pi *(radius * radius * radius);
+   double surface_area = 4 * pi * radius;
+   cout << "Volume = " << sphere_volume;
+   cout << "Area = " << surface_area;
+   return 0;
+}   
